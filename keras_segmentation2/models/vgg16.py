55f4dcaef1187e27167e8553c79e32eaa305286b
ito3tech <h.ito3tech@gmail.com>
2020-04-17 13:17:02 +0900

    upload file 20200417

---
+++ keras_segmentation2/models/vgg16.py
@@ -0,0 +1,172 @@
+import keras
+from keras.models import *
+from keras.layers import *
+import tensorflow.keras.activations as activations
+
+from .config import IMAGE_ORDERING
+import cv2
+
+if IMAGE_ORDERING == 'channels_first':
+	pretrained_url = "https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_th_dim_ordering_th_kernels_notop.h5"
+elif IMAGE_ORDERING == 'channels_last':
+	pretrained_url = "https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5"
+
+
+def get_vgg_encoder( input_height=224 ,  input_width=224 , pretrained='imagenet'):
+
+	assert input_height%32 == 0
+	assert input_width%32 == 0
+
+	if IMAGE_ORDERING == 'channels_first':
+		img_input = Input(shape=(3,input_height,input_width))
+	elif IMAGE_ORDERING == 'channels_last':
+		img_input = Input(shape=(input_height,input_width , 3 ))
+
+	x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', data_format=IMAGE_ORDERING )(img_input)
+	x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2', data_format=IMAGE_ORDERING )(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool', data_format=IMAGE_ORDERING )(x)
+	f1 = x
+	# Block 2
+	x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2', data_format=IMAGE_ORDERING )(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool', data_format=IMAGE_ORDERING )(x)
+	f2 = x
+
+	# Block 3
+	x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3', data_format=IMAGE_ORDERING )(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool', data_format=IMAGE_ORDERING )(x)
+	f3 = x
+
+	# Block 4
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3', data_format=IMAGE_ORDERING )(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool', data_format=IMAGE_ORDERING )(x)
+	f4 = x
+
+	# Block 5
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3', data_format=IMAGE_ORDERING )(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool', data_format=IMAGE_ORDERING )(x)
+	f5 = x
+
+	
+	if pretrained == 'imagenet':
+		VGG_Weights_path = keras.utils.get_file( pretrained_url.split("/")[-1] , pretrained_url  )
+		Model(  img_input , x  ).load_weights(VGG_Weights_path)
+
+
+	return img_input , [f1 , f2 , f3 , f4 , f5 ]
+
+#mod for bayesian nn
+def create_dropout_encoder(input_height=224 ,  input_width=224 , pretrained='imagenet',dropout_rate = 0):
+	assert input_height%32 == 0
+	assert input_width%32 == 0
+
+	if IMAGE_ORDERING == 'channels_first':
+		img_input = Input(shape=(3,input_height,input_width))
+	elif IMAGE_ORDERING == 'channels_last':
+		img_input = Input(shape=(input_height,input_width , 3 ))
+
+	x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', data_format=IMAGE_ORDERING )(img_input)
+	x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2', data_format=IMAGE_ORDERING )(x)
+	x = ( BatchNormalization())(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool', data_format=IMAGE_ORDERING )(x)
+	f1 = x
+	# Block 2
+	x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2', data_format=IMAGE_ORDERING )(x)
+	x = ( BatchNormalization())(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool', data_format=IMAGE_ORDERING )(x)
+	f2 = x
+
+	# Block 3
+	x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3', data_format=IMAGE_ORDERING )(x)
+	x = ( BatchNormalization())(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool', data_format=IMAGE_ORDERING )(x)
+	x = Dropout(dropout_rate,name="block3_dropout")(x, training=True)
+	f3 = x
+
+	# Block 4
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3', data_format=IMAGE_ORDERING )(x)
+	x = ( BatchNormalization())(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool', data_format=IMAGE_ORDERING )(x)
+	x = Dropout(dropout_rate,name="block4_dropout")(x, training=True)
+	f4 = x
+
+	# Block 5
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3', data_format=IMAGE_ORDERING )(x)
+	x = ( BatchNormalization())(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool', data_format=IMAGE_ORDERING )(x)
+	x = Dropout(dropout_rate,name="block5_dropout")(x, training=True)
+	f5 = x
+
+	
+	#if pretrained == 'imagenet':
+	#	VGG_Weights_path = keras.utils.get_file( pretrained_url.split("/")[-1] , pretrained_url  )
+	#	Model(  img_input , x  ).load_weights(VGG_Weights_path)
+
+
+	return img_input , [f1 , f2 , f3 , f4 , f5 ]
+
+#Add grayscale image channel to bayes nn
+def my_encoder(input_height=224 ,  input_width=224 , pretrained='imagenet',dropout_rate = 0):
+	assert input_height%32 == 0
+	assert input_width%32 == 0
+
+	if IMAGE_ORDERING == 'channels_first':
+		img_input = Input(shape=(4,input_height,input_width))
+	elif IMAGE_ORDERING == 'channels_last':
+		img_input = Input(shape=(input_height,input_width , 4 ))
+		
+
+	x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', data_format=IMAGE_ORDERING )(img_input)
+	x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2', data_format=IMAGE_ORDERING )(x)
+	#x = ( BatchNormalization())(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool', data_format=IMAGE_ORDERING )(x)
+	f1 = x
+	# Block 2
+	x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2', data_format=IMAGE_ORDERING )(x)
+	#x = ( BatchNormalization())(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool', data_format=IMAGE_ORDERING )(x)
+	#x = Dropout(dropout_rate,name="block2_dropout")(x, training=True)
+	f2 = x
+
+	# Block 3
+	x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3', data_format=IMAGE_ORDERING )(x)
+	#x = ( BatchNormalization())(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool', data_format=IMAGE_ORDERING )(x)
+	x = Dropout(dropout_rate,name="block3_dropout")(x, training=True)
+	f3 = x
+
+	# Block 4
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3', data_format=IMAGE_ORDERING )(x)
+	#x = ( BatchNormalization())(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool', data_format=IMAGE_ORDERING )(x)
+	x = Dropout(dropout_rate,name="block4_dropout")(x, training=True)
+	f4 = x
+
+	# Block 5
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3', data_format=IMAGE_ORDERING )(x)
+	#x = ( BatchNormalization())(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool', data_format=IMAGE_ORDERING )(x)
+	x = Dropout(dropout_rate,name="block5_dropout")(x, training=True)
+	f5 = x
+
+	return img_input , [f1 , f2 , f3 , f4 , f5 ]
